b4a1377b8d678e752b8e3ef6471ff3c0e87b0d7e
Alesi Mattia <alesimattia@gmail.com>
2021-06-12 10:23:17 +0200

    Dynamic light working
    ESP-01 pinout diagram was WRONG
    - Correctly working dynamic light and reverse light
    - Need to define the indicator-light duty-cycle by sampling on the car

--- Dynamic_indicator_light.ino
+++ Dynamic_indicator_light.ino
@@ -1,5 +1,5 @@
-//#define ESP8266
-//#define FASTLED_ESP8266_RAW_PIN_ORDER
+#define ESP8266
+#define FASTLED_ESP8266_RAW_PIN_ORDER
 #include <FastLED.h>
 
 /** In my Audi A5 I will use the same led strip for the 
@@ -9,16 +9,16 @@
  *  Anyway I'm writing the code in a more general way for applying in various use-cases.
  **/
 
-#define ind_in 0  //gpio1
-#define ind_data 4
-#define ind_count 30
-#define blink_time 650 //ms
-CRGB ind_leds[ind_count]; //0 means first stripe
+#define ind_in 1
+#define ind_data 0
+#define ind_count 20
+#define blink_time 550 //ms
+CRGB ind_leds[ind_count];
 
 /*----------- Reverse Light -----------*/
-#define rev_in 1
-#define rev_data 4
-#define rev_count 24
+#define rev_in 2
+#define rev_data 0  //same pin of ind_data for me
+#define rev_count 50
 CRGB rev_leds[rev_count];
 
 #define COLOR_ORDER RGB
@@ -30,13 +30,12 @@ void setup()
     pinMode(ind_in, INPUT);
     pinMode(ind_data, OUTPUT);
     pinMode(rev_in, INPUT);
-    pinMode(rev_data, OUTPUT);  //same pin of ind_data for me
+    pinMode(rev_data, OUTPUT);  
 
-    FastLED.addLeds<WS2812B, ind_data, COLOR_ORDER>
+    FastLED.addLeds<WS2812, ind_data, COLOR_ORDER>
                     (ind_leds, ind_count);
-    FastLED.addLeds<WS2812B, rev_data, COLOR_ORDER>
+    FastLED.addLeds<WS2812, rev_data, COLOR_ORDER>
                     (rev_leds, rev_count);
-    Serial.println("Leds configured");
 
 }
 
@@ -46,12 +45,12 @@ void loop()
     int rev_on = digitalRead(rev_in);
     Serial.println("Indicator ON? "+ (String)ind_on);
     Serial.println("Reverse ON? "+ (String)ind_on);
-
-    /*if( digitalRead(ind_in) == HIGH )
+    
+    /*if( ind_on == HIGH )
         blink();
-    else  reverse();
-    */
-   reverse();
+    else  reverse();*/
+    Serial.println("---------------");
+    blink();
 }
 
 void blink()
@@ -59,22 +58,24 @@ void blink()
     FastLED.clear(true);
     for(byte i=0; i<ind_count; i++){
         ind_leds[i].setRGB(255, 65, 0);
-        ind_leds[i].maximizeBrightness();
             
         FastLED.show();
-        delay((blink_time-150)/ind_count);
+        delay( (blink_time-150)/ind_count);
     }
-    delay(150); //waits a bit with the full bar enlighted
-    //then the car relay will automatically shut-down the system
+    delay(150);  
+    /*waits a bit with the full bar enlighted
+        then the car relay will automatically shut-down the system */
+    
 }
 
 void reverse()
 {
-    while(true){    //unless the car relay shuts down the power
-        for(byte i=0; i<ind_count; i++){
-            rev_leds[i].setRGB(255, 255, 200);
-            rev_leds[i].maximizeBrightness();
-        }
-        FastLED.show();
+    FastLED.clear(true);
+    for(byte i=0; i<rev_count; i++){
+        rev_leds[i].setRGB(255, 255, 210);
+        //rev_leds[i] = CRGB::White;
+        rev_leds[i].maximizeBrightness();
     }
+    FastLED.show();
+    while(true){ delay(INT32_MAX); }   //unless the car relay shuts down the power}
 }
